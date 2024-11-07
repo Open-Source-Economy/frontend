fff9faef13dd17a4e625507f137666fa462f15c4
Lauriane Mollier <laurianemollier@Laurianes-MacBook-Pro.local>
2024-11-07 12:19:42 +0100

    Home add all section that was commented

--- src/views/pages/website/home/Home.tsx
+++ src/views/pages/website/home/Home.tsx
@@ -3,6 +3,8 @@ import Sec2 from "./elements/Sec2/Sec2";
 import Sec1 from "./elements/Sec1/Sec1";
 import { PageWrapper } from "../../PageWrapper";
 import "./Home.css";
+import Sec3 from "src/views/pages/website/home/elements/Sec3/Sec3";
+import Sec4 from "src/views/pages/website/home/elements/Sec4/Sec4";
 
 interface HomeProps {}
 
@@ -14,14 +16,14 @@ export function Home(props: HomeProps) {
           <Sec1 />
         </div>
         <Sec2 />
-        {/*<div>*/}
-        {/*  <div className="sec3bg1">*/}
-        {/*    <div className="sec3bg2">*/}
-        {/*      <Sec3 />*/}
-        {/*    </div>*/}
-        {/*  </div>*/}
-        {/*  <Sec4 />*/}
-        {/*</div>*/}
+        <div>
+          <div className="sec3bg1">
+            <div className="sec3bg2">
+              <Sec3 />
+            </div>
+          </div>
+          <Sec4 />
+        </div>
       </div>
     </PageWrapper>
   );
