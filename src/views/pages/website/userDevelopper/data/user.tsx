12ff7c63658ea1ba707f9d38e5a8a6636df6f748
suneel kumar <kumarsuneel162@gmail.com>
2024-11-13 18:04:49 +0530

    adding code

--- src/views/pages/website/userDevelopper/data/user.tsx
+++ src/views/pages/website/userDevelopper/data/user.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import img2 from "src/assets/icon/sec2img3.png";
 import img3 from "src/assets/icon/userimg2.png";
 import img4 from "src/assets/icon/userimg1.png";
+import img1 from "src/assets/source.png";
+import bgText from "src/assets/bg-text-2.png";
+import devBg from "src/assets/developer-bg.png";
 import "aos/dist/aos.css";
 import { SolutionProps } from "../elements/Solution";
 import primaryProblemsSrc from "src/assets/btn.png";
@@ -20,7 +23,7 @@ const solution1 = {
   comming: <p className="mb-2">AVAILABLE</p>,
   text: (
     <>
-      <ul className="px-10">
+      <ul className="px-10 mt-[35px]">
         <li className={"list-disc"}>Get technical support</li>
         <li className={"list-disc"}>Discuss your features & needs</li>
         <li className={"list-disc"}>Share your requests.</li>
@@ -42,14 +45,14 @@ const solution2 = {
   comming: <p className="!pb-2.5 flex">COMING SOON</p>,
   subTitle: "Forget the Far West!",
   text: (
-    <>
+    <div className="mt-[45px]">
       <p>
         Be part of a clear, transparent, and <br /> decentralized governance.
       </p>
       <p className="mt-6">
         Shape the evolution and future of <br /> projects that matter to you.
       </p>
-    </>
+    </div>
   ),
 } as SolutionProps;
 
@@ -61,16 +64,16 @@ const solution3 = {
   comming: <p className="!mb-2.5 flex">COMING SOON</p>,
   subTitle: (
     <>
-      Secure your platform, <br /> Secure your business.
+      <p>Secure your platform</p> <p className="!mt-2">Secure your business.</p>
     </>
   ),
   text: (
-    <>
+    <div className="xl:mt-16 md:mt-10 mt-7">
       <p>
         Support, invest, or donate to projects shaping <br /> your future.
       </p>
       <p className="mt-6">Become confident in using open source.</p>
-    </>
+    </div>
   ),
 } as SolutionProps;
 
@@ -83,30 +86,18 @@ export const userProps: UserDeveloperProps = {
       you a headache?
     </>
   ),
-  subtittle1: (
-    <>
-      As an open source dev
-      <br />
-      are you experiencing....
-    </>
-  ),
-  subtittle2: (
-    <>
-      As an open source dev
-      <br />
-      are you experiencing....
-    </>
-  ),
+  subtittle1: <p className="bg-[#FF518C] lg:!p-[15px] mx:p-2 p-1 rounded-[10px] max-w-[319px] w-full">Long-lasting project</p>,
+  subtittle2: <p className="bg-[#FF518C] lg:!p-[15px] mx:p-2 p-1 rounded-[10px] max-w-[319px] w-full">No technical support</p>,
   subtittle3: (
-    <>
-      As an open source dev
+    <p className="bg-[#FF518C] lg:!p-[15px] mx:p-2 p-1 rounded-[10px] max-w-[319px] w-full">
+      Playground for them
       <br />
-      are you experiencing....
-    </>
+      business for me
+    </p>
   ),
-  bgTextImg: img4,
-  mainBg: img4,
-  cartoonImg: img4,
+  bgTextImg: bgText,
+  mainBg: devBg,
+  cartoonImg: img1,
   primaryProblemsSrc,
   secondaryProblemsSrc,
   solutions: [solution1, solution2, solution3],
--- src/views/pages/website/userDevelopper/elements/Problem.tsx
+++ src/views/pages/website/userDevelopper/elements/Problem.tsx
@@ -18,10 +18,10 @@ export function Problem(props: ProblemProps) {
   return (
     <>
       <h1 className="text-md md:text-3xl lg:text-4xl xl:text-[60px] xl:leading-[65px] text-white font-michroma relative z-10">{props.title}</h1>
-      <div className=" max-w-[1740px] w-full mx-auto relative ">
+      <div className="max-w-[1740px] w-full mx-auto relative ">
         <img className="absolute -z-10 pointer-events-auto object-center h-full bottom-0 right-0" src={props.bgTextImg} alt="bgText" />
         <div className="flex items-center justify-between mx:gap-8 sm:gap-4 gap-3 max-w-[1290px] w-full mx-auto relative pt-14">
-          <div className="flex items-center justify-center w-full lg:p-[15px] mx:p-2 p-1 xl:text-[23px] xl:text-lg lg:text-base mx:text-sm text-xs font-michroma rounded-[10px] bg-[#FF7E4B] mx:max-w-[319px] relative z-10">
+          <div className="flex items-center justify-center w-full  xl:text-[23px] xl:text-lg lg:text-base mx:text-sm text-xs font-michroma mx:max-w-[319px] relative z-10">
             {props.subtittle1}
           </div>
           <img className="absolute -z-10 w-full h-[200%] -top-[30%] object-center" src={props.mainBg} alt="mainBg" />
@@ -30,11 +30,11 @@ export function Problem(props: ProblemProps) {
             alt="cartoonImg"
             className="lg:max-w-[390px] md:max-w-[300px] mx:max-w-[220px] sm:max-w-[150px] max-w-[120px] w-full xl:min-h-[550px] mx:min-h-[400px] sm:min-h-[310px] min-h-[250px] h-full object-center z-10 relative"
           />
-          <div className="mx:max-w-[319px] w-full lg:space-y-[194px] mx:space-y-32 space-y-20  h-full relative z-10">
-            <div className="flex items-center justify-center w-full lg:p-[15px] mx:p-2 p-1 xl:text-[23px] xl:text-lg lg:text-base mx:text-sm text-xs font-michroma rounded-[10px] bg-[#FF7E4B] ">
+          <div className="mx:max-w-[319px] w-full lg:space-y-[194px] mx:space-y-32 space-y-20 h-full relative z-10">
+            <div className="flex items-center justify-center w-full  xl:text-[23px] xl:text-lg lg:text-base mx:text-sm text-xs font-michroma ">
               {props.subtittle2}
             </div>
-            <div className="flex items-center justify-center w-full lg:p-[15px] mx:p-2 p-1 xl:text-[23px] xl:text-lg lg:text-base mx:text-sm text-xs font-michroma rounded-[10px] bg-[#FF7E4B]">
+            <div className="flex items-center justify-center w-full  xl:text-[23px] xl:text-lg lg:text-base mx:text-sm text-xs font-michroma">
               {props.subtittle3}
             </div>
           </div>
