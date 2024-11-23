0a3efaf492ab898c62409b27048a759655cc5f76
Lauriane Mollier <laurianemollier@Laurianes-MacBook-Pro.local>
2024-11-23 14:00:23 +0100

    Vercel: Should fix projects on home

--- src/views/pages/website/home/elements/Projects.tsx
+++ src/views/pages/website/home/elements/Projects.tsx
@@ -30,11 +30,11 @@ export function Projects(props: ProjectsProps) {
   ];
   return (
     <>
-      <div className="relative sm:px-8 px-[0px] flex w-full items-center justify-center pb-[100px] max-[540px]:pt-12 pt-[40px] lg:pb-[200px] lg:pt-[100px]">
-        <div className="dig-into-details relative flex w-full flex-col items-center justify-center text-center gap-[80px] lg:gap-[130px]">
+      <div className="relative sm:px-8 max-[540px]:px-[18px] flex w-full items-center justify-center pb-[100px] max-[540px]:pt-12 pt-[40px] lg:pb-[200px] lg:pt-[100px] max-w-[1330px]   mx-auto">
+        <div className="dig-into-details relative flex !max-w-[1320px] !w-full flex-col items-center justify-center text-center gap-[80px] lg:gap-[130px]">
           <img src={backdropSVG} className="pointer-events-none absolute top-[0px] z-0" alt="" />
           <h1 className="text-center font-mich text-[28px] font-[400] lg:text-[42px]">Latest Open Source Projects</h1>
-          <div className="dig-grid z-[10] grid w-full px-[20px] lg:grid-cols-3 lg:px-0">
+          <div className="flex flex-wrap z-[10]  w-full gap-4 justify-center ">
             {cardsProps.map((cardProps, index) => (
               <>
                 <Cards key={index} {...cardProps} />
