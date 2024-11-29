87e9905619a8d517ee644e37f4721f16b9480f92
star-works <111379816+star-works@users.noreply.github.com>
2024-11-29 16:56:44 +0530

    fix spaces and class

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -11,29 +11,29 @@ export function cn(...inputs: ClassValue[]) {
 const AUDIENCE_COLORS = {
   ALL: {
     primary:
-      "bg-gradient-to-r from-[#FF7E4B]  via-[#FF518C] to-[#66319B] h-[61px] min-w-[210px]  after:absolute after:w-[98%] after:top-1/2 after:left-1/2 after:bg-secondary after:h-[93%] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-md after:opacity-0 after:duration-300 rounded-md",
+      "bg-gradient-to-r from-[#FF7E4B] via-[#FF518C] to-[#66319B] h-[61px] min-w-[210px] after:absolute after:w-[98%] after:top-1/2 after:left-1/2 after:bg-secondary after:h-[93%] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-md after:opacity-0 after:duration-300 rounded-md",
     secondary:
-      "bg-gradient-to-r from-[#FF7E4B]  via-[#FF518C] text-white to-[#66319B] text-white z-10     after:absolute after:w-[98%] after:top-1/2 after:left-1/2 after:bg-secondary after:h-[93%] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-md  after:opacity-100 after:duration-300 rounded-md",
+      "bg-gradient-to-r from-[#FF7E4B] via-[#FF518C] text-white to-[#66319B] text-white z-10 after:absolute after:w-[98%] after:top-1/2 after:left-1/2 after:bg-secondary after:h-[93%] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-md  after:opacity-100 after:duration-300 rounded-md",
     hoverPrimary: "after:hover:opacity-100 hover:bg-transparent",
     hoverSecondary: "after:hover:opacity-0 hover:bg-transparent",
   },
   USER: {
-    primary: "bg-primary-user border-primary-user border-[2px]",
-    secondary: "border-primary-user border-[2px] ",
-    hoverPrimary: "hover:bg-transparent  ease-in-out transition-all",
+    primary: "bg-primary-user border-primary-user border-2",
+    secondary: "border-primary-user border-2",
+    hoverPrimary: "hover:bg-transparent ease-in-out transition-all",
     hoverSecondary: "hover:bg-primary-user",
   },
   DEVELOPER: {
-    primary: "bg-primary-developer border-primary-developer border-[2px]",
-    secondary: "border-primary-developer border-[2px]",
+    primary: "bg-primary-developer border-primary-developer border-2",
+    secondary: "border-primary-developer border-2",
     hoverPrimary: "hover:bg-transparent  ease-in-out transition-all",
     hoverSecondary: "hover:bg-primary-developer  ease-in-out transition-all",
   },
   STAKEHOLDER: {
-    primary: "bg-primary-stakeholder border-primary-stakeholder border-[2px]",
-    secondary: "border-primary-stakeholder border-[2px]",
-    hoverPrimary: "hover:bg-transparent transition-all  ease-in-out transition-all",
-    hoverSecondary: "hover:bg-primary-stakeholder transition-all  ease-in-out transition-all",
+    primary: "bg-primary-stakeholder border-primary-stakeholder border-2",
+    secondary: "border-primary-stakeholder border-2",
+    hoverPrimary: "hover:bg-transparent transition-all ease-in-out transition-all",
+    hoverSecondary: "hover:bg-primary-stakeholder transition-all ease-in-out transition-all",
   },
 } as const;
 
