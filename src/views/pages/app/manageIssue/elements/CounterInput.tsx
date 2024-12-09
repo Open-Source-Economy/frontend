fe4ebf30f404c358eaad9dee6239fbec4d30bb0f
Lauriane Mollier <laurianemollier@Laurianes-MacBook-Pro.local>
2024-12-09 18:33:20 +0100

    Fix design: button, color based on audiance, more mobile friendly, ...

--- src/views/pages/app/manageIssue/elements/CounterInput.tsx
+++ src/views/pages/app/manageIssue/elements/CounterInput.tsx
@@ -1,7 +1,5 @@
-import React, { ChangeEvent } from "react";
-import up from "src/assets/arrowup.png";
-import down from "src/assets/arrowdown.png";
 import Decimal from "decimal.js";
+import { ChangeEvent, MouseEvent } from "react";
 
 interface CounterInputProps {
   value: Decimal | null;
@@ -23,21 +21,27 @@ export function CounterInput(props: CounterInputProps) {
             className="border-0 outline-none md:text-[18px] text-[16px] md:w-24 sm:w-28 w-20 bg-transparent"
           />
           <div className="flex flex-col gap-2">
-            <img src={up} className="md:w-[11px] w-[11px] h-2 cursor-pointer" onClick={props.increment} alt="Increment" />
-            <img
-              src={down}
+            <button className="md:w-[11px] w-[11px] h-2 cursor-pointer" onClick={props.increment}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9" viewBox="0 0 11 9" fill="none">
+                <path d="M5.5 0L10.2631 8.76562H0.73686L5.5 0Z" fill="#D9D9D9" />
+              </svg>
+            </button>
+            <button
               className={`md:w-[11px] w-[11px] h-2 cursor-pointer ${props.value?.isZero() ? "opacity-50 cursor-not-allowed" : ""}`}
               onClick={props.decrement}
-              alt=""
               style={{
                 pointerEvents: props.value?.isZero() ? "none" : "auto",
               }}
-            />
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="11" height="8" viewBox="0 0 11 8" fill="none">
+                <path d="M5.5 8L10.2631 0.265625H0.73686L5.5 8Z" fill="#D9D9D9" />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
       <div>
-        <h2 className="gradient-texts font-bold md:text-[16px] text-[12px]">DoW</h2>
+        <h2 className="gradient-texts select-none font-bold md:text-[16px] text-[12px]">DoW</h2>
         <style>{`
           .gradient-texts {
             background: linear-gradient(90deg, #ff7e4b, #ff518c);
