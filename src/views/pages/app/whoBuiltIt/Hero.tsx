928677fc321607491e6123a9cc945afa1508d5a3
Tanu chaudhary <kaurtanu213@gmail.com>
2024-12-09 20:37:51 +0530

    fix background issues in who-built-it

--- src/views/pages/app/whoBuiltIt/Hero.tsx
+++ src/views/pages/app/whoBuiltIt/Hero.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { DonutIcon, RightDonutIcon } from "src/Utils/Icons";
+import whoBuiltHero from "src/assets/Rectangle.png";
 
 const Hero = () => {
   return (
     <>
-      <section className="relative w-full pt-20 lg:!pt-32">
-        <h1 className="relative  text-center mx-auto min-[1800px]:!mt-[141px]  who-built-it-main-heading bg-gradient-to-r from-[#FF7E4B] via-[#FF518C] to-[#66319B] w-fit text-transparent bg-clip-text">
+      {/* =================== RIGHT DONUT =============== */}
+      <span className="absolute hidden max-w-[200px] xl:block -right-[3%] top-[11%] 2xl:max-w-[240px]">
+        <RightDonutIcon />
+      </span>
+      <section className=" w-full pt-20 lg:!pt-32 3xl:!pt-[141px] pb-10 max-w-[1164px] relative 3xl:max-w-[1376px] !px-4 mx-auto">
+        {/* ================== HERO LINEAR BG ================== */}
+        <img
+          src={whoBuiltHero}
+          alt=""
+          className="absolute pointer-events-none sm:max-w-[80%] w-full lg:max-w-[75%] xl:max-w-[802px] object-cover -translate-x-1/2 left-1/2 z-0 top-[1%] opacity-40 md:opacity-30"
+        />
+
+        {/* ================== RIGHT DONUT ===================== */}
+        <span className="absolute hidden xl:block xl:-left-[6%] 2xl:-left-[10%] top-[4%]  max-w-[200px] 2xl:max-w-[280px]">
+          <DonutIcon />
+        </span>
+        <h1 className="relative  text-center mx-auto   who-built-it-main-heading bg-gradient-to-r from-[#FF7E4B] via-[#FF518C] to-[#66319B] w-fit text-transparent bg-clip-text">
           Open Source
         </h1>
         <h1 className="who-built-it-main-heading max-w-[887px] mx-auto mt-2.5">
@@ -14,12 +31,10 @@ const Hero = () => {
             <span className="absolute w-[30%] bottom-0 left-1/2 -translate-x-1/2 h-[4px] bg-gradient-to-r from-[#FF7E4B] via-[#FF518C] to-[#66319B]"></span>
           </span>{" "}
         </h1>
-        <h3 className="text-lg md:text-2xl lg:text-3xl max-w-[80%] mx-auto xl:text-[33px] text-center font-montserrat min-[1800px]:text-[40px] mt-10 sm:mt-12 md:mt-[60px]">
+        <h3 className="text-lg md:text-2xl lg:text-3xl max-w-[80%] mx-auto xl:text-[33px] text-center font-montserrat 3xl:text-[40px] mt-10 sm:mt-12 md:mt-[60px]">
           Need help with an Open Source project?
         </h3>
-        <h5 className="font-montserrat text-sm md:text-xl text-center min-[1800px]:text-2xl opacity-70 !mt-5">
-          We're the experts who build, debug, and maintain it
-        </h5>
+        <h5 className="font-montserrat text-sm md:text-xl text-center 3xl:text-2xl opacity-70 !mt-5">We're the experts who build, debug, and maintain it</h5>
       </section>
     </>
   );
--- src/views/pages/app/whoBuiltIt/Services.tsx
+++ src/views/pages/app/whoBuiltIt/Services.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ServiceBox from "./ServiceBox";
+import rightLinear from "src/assets/right-linear-bg.png";
 import { services } from "./Helper";
 
 const Services = () => {
   return (
-    <div className="mt-10 lg:mt-[70px] grid grid-cols-1 place-items-center w-full lg:grid-cols-2 !gap-5 xl:!gap-8 min-[1800px]:!gap-10 xl:px-8">
-      {services.map((service, index) => (
-        <ServiceBox key={index} data={service} />
-      ))}
-    </div>
+    <>
+      {" "}
+      <img src={rightLinear} alt="" className="absolute pointer-events-none object-cover opacity-90 right-0 max-w-[671px]  z-0 top-[24%]" />
+      <div className="max-w-[1164px] relative 3xl:max-w-[1376px] !px-4 mx-auto pb-12 lg:pt-10 grid grid-cols-1 place-items-center w-full lg:grid-cols-2 !gap-5 xl:!gap-8 3xl:!gap-10 xl:px-8">
+        {services.map((service, index) => (
+          <ServiceBox key={index} data={service} />
+        ))}
+      </div>
+    </>
   );
 };
 
