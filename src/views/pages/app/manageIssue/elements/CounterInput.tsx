a8f4cb104e664453daf6a308303adb5517ac719a
Tanu chaudhary <kaurtanu213@gmail.com>
2024-12-10 23:18:40 +0530

    fix issues

--- src/views/pages/app/manageIssue/elements/CounterInput.tsx
+++ src/views/pages/app/manageIssue/elements/CounterInput.tsx
@@ -21,7 +21,7 @@ export function CounterInput(props: CounterInputProps) {
             className="border-0 outline-none md:text-[18px] text-[16px] md:w-24 sm:w-28 w-20 bg-transparent"
           />
           <div className="flex flex-col gap-2">
-            <button className="md:w-[11px] w-[11px] h-2 cursor-pointer" onClick={props.increment}>
+            <button type="button" className="md:w-[11px] w-[11px] h-2 cursor-pointer" onClick={props.increment}>
               <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9" viewBox="0 0 11 9" fill="none">
                 <path d="M5.5 0L10.2631 8.76562H0.73686L5.5 0Z" fill="#D9D9D9" />
               </svg>
@@ -29,6 +29,7 @@ export function CounterInput(props: CounterInputProps) {
             <button
               className={`md:w-[11px] w-[11px] h-2 cursor-pointer ${props.value?.isZero() ? "opacity-50 cursor-not-allowed" : ""}`}
               onClick={props.decrement}
+              type="button"
               style={{
                 pointerEvents: props.value?.isZero() ? "none" : "auto",
               }}
