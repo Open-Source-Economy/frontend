67c826d0ef552a1713bf875e4f92193b57c50cf2
Lauriane Mollier <laurianemollier@Laurianes-MacBook-Pro.local>
2024-12-12 13:03:07 +0100

    Fix: Cards: / is not a link

--- src/views/pages/website/home/elements/Cards.tsx
+++ src/views/pages/website/home/elements/Cards.tsx
@@ -26,8 +26,9 @@ export function Cards(props: CardsProps) {
         <div className="max-[860px]:min-h-[150px]">
           <h4 className="font-mich mt-3 text-2xl max-[540px]:!text-1xl">
             <ExternalLink href={props.owner.htmlUrl} className="duration-300 text-secondary">
-              {props.owner.id.login !== props.repository.id.name && <span>{props.owner.id.login}/</span>}
+              {props.owner.id.login !== props.repository.id.name && <span>{props.owner.id.login}</span>}
             </ExternalLink>
+            <span className="text-secondary">/</span>
 
             <ExternalLink href={props.repository.htmlUrl} className="duration-300">
               {props.repository.id.name}
