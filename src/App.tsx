a6340abe3671b6a85a46f0e1dc68a6434c3705ee
Tanu chaudhary <kaurtanu213@gmail.com>
2024-12-17 14:15:21 +0530

    laoder  change settiming true to false

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ const App = () => {
   useEffect(() => {
     // Simulate a loading delay or fetch initial app data here
     const timer = setTimeout(() => {
-      setIsLoading(true);
+      setIsLoading(false);
     }, 3000);
 
     return () => clearTimeout(timer); // Cleanup
