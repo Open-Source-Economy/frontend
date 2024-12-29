a1d3575944a576eed5e8d82a18e2f04c9ba93ac3
tronexis <tanjilahmed7575@gmail.com>
2024-12-29 21:18:55 +0600

    Feat: add loading ui in Projects component

--- src/views/pages/website/home/elements/Cards.tsx
+++ src/views/pages/website/home/elements/Cards.tsx
@@ -5,14 +5,34 @@ import { Owner, Repository } from "../../../../../model";
 import { Audience } from "../../../../Audience";
 
 export interface CardsProps {
-  owner: Owner;
-  repository: Repository;
-  audience: Audience;
-  action?: string;
-  to?: To;
+  owner?: Owner;
+	repository?: Repository;
+	audience?: Audience;
+	action?: string;
+	to?: To;
+	isLoading?: boolean;
 }
 
 export function Cards(props: CardsProps) {
+  if (props.isLoading) {
+		return (
+			<div className="flex w-[386px] flex-col items-center justify-between gap-y-3 rounded-[40px] bg-[#14233A] py-8 pb-4 pt-4 max-[540px]:w-full max-[490px]:pb-10 animate-pulse">
+				<div className="flex flex-col items-center justify-between w-full">
+					<div className="size-[85px] rounded-full bg-gray-700/50" />
+					<div className="w-full space-y-3 mt-4">
+						<div className="h-6 bg-gray-700/50 rounded-full w-2/3 mx-auto" />
+						<div className="space-y-2 px-4">
+							<div className="h-4 bg-gray-700/50 rounded-full w-full" />
+							<div className="h-4 bg-gray-700/50 rounded-full w-4/5" />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	if (!props.owner || !props.repository) return null;
+  
   const displayOwner = props.owner.id.login !== props.repository.id.name;
   return (
     <div
