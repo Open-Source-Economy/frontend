233400c69a0e28b11a0894df3c55166d2dbd41d8
Lauriane Mollier <lauriane@open-source-economy.com>
2025-02-07 15:59:52 +0100

    Fix: campaign page background and button slow to click on mobile

--- src/views/pages/app/campaign/elements/AQuestion.tsx
+++ src/views/pages/app/campaign/elements/AQuestion.tsx
@@ -1,4 +1,3 @@
-import { LeftLinear } from "src/Utils/Icons";
 import React from "react";
 import { CampaignDescription } from "../../../../../dtos";
 
@@ -11,10 +10,7 @@ export function AQuestion(props: AQuestionProps) {
   return (
     <section className="!px-4 pb-16 3xl:pb-24 pt-16 3xl:pt-24 relative flex flex-col">
       <div className="bg-sunset-glow-gradient hidden xl:block !max-w-[490px] w-full min-h-[600px] h-full 3xl:min-h-[906px] max-h-[906px] absolute -right-10 3xl:right-[1%] -top-[30%] 3xl:-top-[50%] blur-[125px] -z-20 opacity-20 shrink-0 pointer-events-none !-rotate-[120deg] rounded-full"></div>
-      <div className="bg-gradient-to-l from-[#5935A1] to-[#AC3556] min-h-[500px] 3xl:min-h-[906px] max-h-[1056px] min-w-[280px] max-w-[364px] w-full h-full absolute right-[-3%] 3xl:right-[6%] -top-[20%]  3xl:-top-[60%] blur-[125px] -z-0 opacity-50 shrink-0 pointer-events-none -rotate-[41.351deg] rounded-[1057px]"></div>
-      <span className="absolute -left-[10%] top-[70%] lg:-top-[80%] -z-10">
-        <LeftLinear />
-      </span>
+      <div className="bg-gradient-to-l from-[#5935A1] to-[#AC3556] min-h-[500px] 3xl:min-h-[906px] max-h-[1056px] min-w-[280px] max-w-[364px] w-full h-full absolute right-[-3%] 3xl:right-[6%] -top-[20%]  3xl:-top-[60%] blur-3xl sm:blur-[125px] -z-0 opacity-40 sm:opacity-50 shrink-0 pointer-events-none -rotate-[41.351deg] rounded-[1057px]"></div>
 
       {/*TODO: use some of the refactored titles*/}
       <h2 className="section-heading relative z-20 !pb-3 lg:!pb-5 3xl:!pb-8 max-w-[386px] md:max-w-[470px] w-full mx-auto lg:max-w-full">
