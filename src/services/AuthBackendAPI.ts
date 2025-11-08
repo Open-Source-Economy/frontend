d4a8b7b42b17a774d06da7e5b69102ed509d2c15
Lauriane Mollier <lauriane@open-source-economy.com>
2025-11-08 18:03:28 +0100

    Refactor: AuthBackendAPI

--- src/services/AuthBackendAPI.ts
+++ src/services/AuthBackendAPI.ts
@@ -1,44 +1,25 @@
-import axios, { AxiosInstance } from "axios";
-import {
-  GetCompanyUserInviteInfoQuery,
-  GetCompanyUserInviteInfoResponse,
-  GetRepositoryUserInviteInfoQuery,
-  GetRepositoryUserInviteInfoResponse,
-  LoginBody,
-  LoginQuery,
-  LoginResponse,
-  RegisterBody,
-  RegisterQuery,
-  RegisterResponse,
-  StatusResponse,
-} from "@open-source-economy/api-types";
-import { AuthBackendAPIMock } from "src/__mocks__";
+import { AxiosInstance } from "axios";
+import * as dto from "@open-source-economy/api-types";
+import { api, handleError } from "./index";
+import { AuthBackendAPIMock } from "src/__mocks__/AuthBackendAPI.mock";
 import { ApiError } from "src/ultils/error/ApiError";
 import { config } from "src/ultils";
-import { handleError } from "./index";
 
 export function getAuthBackendAPI(): AuthBackendAPI {
   if (config.api.useMock) {
     return new AuthBackendAPIMock();
-  } else {
-    return new AuthBackendAPIImpl(axios);
   }
+  return new AuthBackendAPIImpl(api);
 }
 
 export interface AuthBackendAPI {
-  checkUserStatus(): Promise<StatusResponse | ApiError>;
-
-  login(body: LoginBody, query: LoginQuery): Promise<LoginResponse | ApiError>;
-
-  register(body: RegisterBody, query: RegisterQuery): Promise<RegisterResponse | ApiError>;
-
+  checkUserStatus(): Promise<dto.StatusResponse | ApiError>;
+  login(body: dto.LoginBody, query: dto.LoginQuery): Promise<dto.LoginResponse | ApiError>;
+  register(body: dto.RegisterBody, query: dto.RegisterQuery): Promise<dto.RegisterResponse | ApiError>;
   loginWithGitHub(redirectPath?: string): void;
-
   deleteSession(): Promise<void | ApiError>;
-
-  getCompanyUserInviteInfo(query: GetCompanyUserInviteInfoQuery): Promise<GetCompanyUserInviteInfoResponse | ApiError>;
-
-  getRepositoryUserInviteInfo(query: GetRepositoryUserInviteInfoQuery): Promise<GetRepositoryUserInviteInfoResponse | ApiError>;
+  getCompanyUserInviteInfo(query: dto.GetCompanyUserInviteInfoQuery): Promise<dto.GetCompanyUserInviteInfoResponse | ApiError>;
+  getRepositoryUserInviteInfo(query: dto.GetRepositoryUserInviteInfoQuery): Promise<dto.GetRepositoryUserInviteInfoResponse | ApiError>;
 }
 
 class AuthBackendAPIImpl implements AuthBackendAPI {
@@ -48,30 +29,36 @@ class AuthBackendAPIImpl implements AuthBackendAPI {
     this.api = api;
   }
 
-  async checkUserStatus(): Promise<StatusResponse | ApiError> {
-    return handleError<StatusResponse>(() => this.api.get(`${config.api.url}/auth/status`, { withCredentials: true }), "checkUserStatus");
+  async checkUserStatus(): Promise<dto.StatusResponse | ApiError> {
+    return handleError<dto.StatusResponse>(() => this.api.get(`${config.api.url}/auth/status`, { withCredentials: true }), "checkUserStatus");
   }
 
-  async login(body: LoginBody, query: LoginQuery): Promise<LoginResponse | ApiError> {
-    return handleError(() => this.api.post(`${config.api.url}/auth/login`, body, { withCredentials: true }), "login");
+  async login(body: dto.LoginBody, query: dto.LoginQuery): Promise<dto.LoginResponse | ApiError> {
+    return handleError<dto.LoginResponse>(() => this.api.post(`${config.api.url}/auth/login`, body, { withCredentials: true, params: query }), "login");
   }
 
-  async register(body: RegisterBody, query: RegisterQuery): Promise<RegisterResponse | ApiError> {
-    if (query.companyToken) {
-      const queryParams = `companyToken=${encodeURIComponent(query.companyToken)}`;
-      return handleError(
-        () => this.api.post(`${config.api.url}/auth/register-as-company?${queryParams}`, body, { withCredentials: true }),
+  async register(body: dto.RegisterBody, query: dto.RegisterQuery): Promise<dto.RegisterResponse | ApiError> {
+    const { companyToken, repositoryToken } = query;
+
+    if (companyToken) {
+      return handleError<dto.RegisterResponse>(
+        () => this.api.post(`${config.api.url}/auth/register-as-company`, body, { withCredentials: true, params: { companyToken } }),
         "register-as-company",
       );
-    } else if (query.repositoryToken) {
-      const queryParams = `repositoryToken=${encodeURIComponent(query.repositoryToken)}`;
-      return handleError(
-        () => this.api.post(`${config.api.url}/auth/register-as-maintainer?${queryParams}`, body, { withCredentials: true }),
+    } else if (repositoryToken) {
+      return handleError<dto.RegisterResponse>(
+        () =>
+          this.api.post(`${config.api.url}/auth/register-as-maintainer`, body, {
+            withCredentials: true,
+            params: { repositoryToken },
+          }),
         "register-as-maintainer",
       );
-    } else {
-      return handleError(() => this.api.post(`${config.api.url}/auth/register`, body, { withCredentials: true }), "register");
     }
+    return handleError<dto.RegisterResponse>(
+      () => this.api.post(`${config.api.url}/auth/register`, body, { withCredentials: true, params: query }),
+      "register",
+    );
   }
 
   loginWithGitHub(redirectPath?: string): void {
@@ -79,22 +66,19 @@ class AuthBackendAPIImpl implements AuthBackendAPI {
   }
 
   async deleteSession(): Promise<void | ApiError> {
-    return handleError(() => this.api.post(`${config.api.url}/auth/logout`, {}, { withCredentials: true }), "deleteSession");
+    return handleError<void>(() => this.api.post(`${config.api.url}/auth/logout`, {}, { withCredentials: true }), "deleteSession");
   }
 
-  async getCompanyUserInviteInfo(query: GetCompanyUserInviteInfoQuery): Promise<GetCompanyUserInviteInfoResponse | ApiError> {
-    // TODO: make that generic for all the params
-    const queryParams = `token=${encodeURIComponent(query.token)}`;
-    return handleError<GetCompanyUserInviteInfoResponse>(
-      () => this.api.get(`${config.api.url}/auth/company-user-invite-info?${queryParams}`, { withCredentials: true }),
+  async getCompanyUserInviteInfo(query: dto.GetCompanyUserInviteInfoQuery): Promise<dto.GetCompanyUserInviteInfoResponse | ApiError> {
+    return handleError<dto.GetCompanyUserInviteInfoResponse>(
+      () => this.api.get(`${config.api.url}/auth/company-user-invite-info`, { withCredentials: true, params: query }),
       "getCompanyUserInviteInfo",
     );
   }
 
-  async getRepositoryUserInviteInfo(query: GetRepositoryUserInviteInfoQuery): Promise<GetRepositoryUserInviteInfoResponse | ApiError> {
-    const queryParams = `token=${encodeURIComponent(query.token)}`;
-    return handleError<GetRepositoryUserInviteInfoResponse>(
-      () => this.api.get(`${config.api.url}/auth/repository-user-invite-info?${queryParams}`, { withCredentials: true }),
+  async getRepositoryUserInviteInfo(query: dto.GetRepositoryUserInviteInfoQuery): Promise<dto.GetRepositoryUserInviteInfoResponse | ApiError> {
+    return handleError<dto.GetRepositoryUserInviteInfoResponse>(
+      () => this.api.get(`${config.api.url}/auth/repository-user-invite-info`, { withCredentials: true, params: query }),
       "getRepositoryUserInviteInfo",
     );
   }
